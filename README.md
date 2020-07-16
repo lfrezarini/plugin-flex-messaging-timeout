f8a878ce1cd73121bac0109aeea3bd82e1c3b35a
tfrezarini <lfrezarini@twilio.com>
2020-07-16 12:17:48 -0300

    Checking timeout only in accepted tasks and enhancing the README

--- README.md
+++ README.md
@@ -22,9 +22,30 @@ cp .env.example .env
 
 You can run it locally using the `npm start` command.
 
+## Changing the timeout time and the enabled channels to apply it
+
+You can customize the `.env` file to change the maximum time without interaction that a chat can have before it is finished for good, changing the variable `REACT_APP_INACTIVITY_TIMEOUT_IN_MINUTES`. You can also change the channels where the timeout time is applied to modifying the `REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS`.
+
+```bash
+REACT_APP_INACTIVITY_TIMEOUT_IN_MINUTES=15
+REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS=web,whatsapp
+```
+
+The available channels to activate the timeout time is:
+
+* **web**: Enables the timeout time for WebChats.
+* **whatsapp**: Enables the timeout time for WhatsApp chats.
+* **sms**: Enables the timeout time for SMS chats.
+
+If you want to enable for all these three channels, you should define the `REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS` variable as below:
+
+```
+REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS=web,whatsapp,sms
+```
+
 ## Deploying the plugin
 
-Before you deploy the plugin to your flex instance, create a `public/appConfig.js` file based on the `public/appConfig.example.js`. If the file was already created automatically when you ran the plugin locally for the first time, you can ignore this step.
+Before you deploy the plugin to your flex instance, create a `public/appConfig.js` file based on the `public/appConfig.example.js`. If the file was already created automatically when you ran the plugin locally for the first time, you could ignore this step.
 
 ```bash
 cp public/appConfig.example.js public/appConfig.js
--- src/components/TaskTimeout/TaskTimeout.jsx
+++ src/components/TaskTimeout/TaskTimeout.jsx
@@ -3,8 +3,11 @@ import moment from 'moment';
 import * as Flex from '@twilio/flex-ui';
 
 class TaskTimeout extends React.Component {
-  maxInactivityTimeInMinutes = process.env.REACT_APP_INACTIVITY_TIMEOUT_IN_MINUTES;
-  enabledTimeoutChannels = process.env.REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS.trim().split(',');
+  maxInactivityTimeInMinutes =
+    process.env.REACT_APP_INACTIVITY_TIMEOUT_IN_MINUTES;
+  enabledTimeoutChannels = process.env.REACT_APP_INACTIVITY_TIMEOUT_ENABLED_CHANNELS.trim().split(
+    ','
+  );
 
   constructor(props) {
     super(props);
@@ -20,31 +23,39 @@ class TaskTimeout extends React.Component {
     const { task } = this.props;
 
     if (this.enabledTimeoutChannels.includes(task.channelType)) {
+      console.debug(
+        `Starting interaction timeout verification for task ${task.sid} with channel type ${task.channelType}`,
+        task
+      );
       const interval = setInterval(this.verifyInactivityTimeout, 15000);
       this.setState({ interval });
     }
   }
 
   async verifyInactivityTimeout() {
-    const { chatChannel } = this.props;
+    const { chatChannel, task } = this.props;
 
-    if (
-      chatChannel &&
-      chatChannel.source &&
-      chatChannel.source.lastMessage
-    ) {
+    if (task.status !== 'accepted') {
+      console.log(`ignoring '${task.status}' task with SID ${task.sid}`, task);
+      return;
+    }
+
+    if (chatChannel && chatChannel.source && chatChannel.source.lastMessage) {
+      console.debug(`verifying task '${task.sid}' timeout`, task);
       const { source } = chatChannel;
       const { timestamp } = source.lastMessage;
 
-      console.log(process.env)
-
-      console.debug('verifying chatChannel source timeout', source)
+      console.debug('verifying chatChannel source timeout', source);
       const inactivityTimeInMinutes = moment().diff(
         moment(timestamp),
         'minutes'
       );
 
-      console.debug('chatChannel inacitivty time in minutes: ', inactivityTimeInMinutes);
+      console.debug(
+        `Task ${task.sid} chatChannel inactivity time in minutes:`,
+        inactivityTimeInMinutes
+      );
+
       if (inactivityTimeInMinutes >= this.maxInactivityTimeInMinutes) {
         await this.wrapUpAndFinishTask();
       }
@@ -57,12 +68,12 @@ class TaskTimeout extends React.Component {
     try {
       await task.wrapUp();
       await task.complete();
-      
+
       const { name, from } = task.attributes;
 
-      Flex.Notifications.showNotification("taskTimeoutNotification", { 
+      Flex.Notifications.showNotification('taskTimeoutNotification', {
         task,
-        name: name !== "" ? name : from
+        name: name !== '' ? name : from
       });
     } catch (err) {
       console.error(
